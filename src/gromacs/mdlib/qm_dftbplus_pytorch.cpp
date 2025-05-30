4d4aec7b98a56d24fd64ea58666987e546088bc9
Lukas Petersen <lukas.petersen93@yahoo.de>
2025-05-30 16:56:24 +0200

    feat: implement parallel DFTB+ and TensorFlow calculations in QMMM framework

--- src/gromacs/mdlib/qm_dftbplus_pytorch.cpp
+++ src/gromacs/mdlib/qm_dftbplus_pytorch.cpp
@@ -3,6 +3,7 @@
  */
 
 #include "config.h"
+#include "gromacs/math/vec.h"
 #include "gromacs/utility/smalloc.h"
 
 // When not built in a configuration with QMMM support, much of this
@@ -13,6 +14,7 @@
 #if GMX_QMMM_DFTBPLUS_PYTORCH
 
 #include "gromacs/mdlib/qm_dftbplus_pytorch.h"
+#include <omp.h>
 
 void init_dftbplus_pytorch(QMMM_QMrec* qm, QMMM_rec* qr, const t_inputrec* ir, const t_commrec* cr)
 {
@@ -23,30 +25,61 @@ void init_dftbplus_pytorch(QMMM_QMrec* qm, QMMM_rec* qr, const t_inputrec* ir, c
 }
 
 real call_dftbplus_pytorch(QMMM_rec* qr,
-                           const t_commrec* cr,
-                           QMMM_QMrec* qm,
-                           const QMMM_MMrec& mm,
-                           rvec f[],
-                           rvec fshift[],
-                           t_nrnb* nrnb,
-                           gmx_wallcycle_t wcycle)
+                          const t_commrec* cr,
+                          QMMM_QMrec* qm,
+                          const QMMM_MMrec& mm,
+                          rvec f[],
+                          rvec fshift[],
+                          t_nrnb* nrnb,
+                          gmx_wallcycle_t wcycle)
 {
     int nQMatoms = qm->nrQMatoms_get();
-    // Call DFTB+ for base energy/forces
-    real e_dftb = call_dftbplus(qr, cr, qm, mm, f, fshift, nrnb, wcycle);
-    // Store DFTB+ forces for later correction
+    real e_dftb = 0.0;
+    real e_delta = 0.0;
+    
+    // Allocate memory for both force arrays
     rvec* f_dftb = nullptr;
+    rvec* f_delta = nullptr;
     snew(f_dftb, nQMatoms);
+    snew(f_delta, nQMatoms);
+    
+    for (int i=0; i < nQMatoms; i++)
+    {
+        clear_rvec(f_dftb[i]);
+        clear_rvec(f_delta[i]);
+    }
+    
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            // Task for DFTB+ calculation
+            #pragma omp task shared(e_dftb, f_dftb)
+            {
+                // Call DFTB+ for base energy/forces (directly storing in f_dftb)
+                e_dftb = call_dftbplus(qr, cr, qm, mm, f_dftb, fshift, nrnb, wcycle);
+            }
+            
+            // Task for PyTorch calculation
+            #pragma omp task shared(e_delta, f_delta)
+            {
+                // Call PyTorch for delta correction (using f_delta)
+                e_delta = call_pytorch(qr, cr, qm, mm, f_delta, fshift, nrnb, wcycle);
+            }
+            
+            // Wait for both tasks to complete
+            #pragma omp taskwait
+        }
+    }
+    
+    // Combine the forces
     for (int i = 0; i < nQMatoms; ++i)
         for (int d = 0; d < DIM; ++d)
-            f_dftb[i][d] = f[i][d];
-    // Call PyTorch for delta correction
-    real e_delta = call_pytorch(qr, cr, qm, mm, f, fshift, nrnb, wcycle);
-    // Add delta correction to DFTB+ forces
-    for (int i = 0; i < nQMatoms; ++i)
-        for (int d = 0; d < DIM; ++d)
-            f[i][d] += f_dftb[i][d];
+            f[i][d] = f_dftb[i][d] + f_delta[i][d];
+    
     sfree(f_dftb);
+    sfree(f_delta);
+    
     // Return total energy (DFTB+ + delta)
     return e_dftb + e_delta;
 }
--- src/gromacs/mdlib/qm_dftbplus_tensorflow.cpp
+++ src/gromacs/mdlib/qm_dftbplus_tensorflow.cpp
@@ -1,8 +1,9 @@
 /*
- * Hybrid DFTB+ + PyTorch QMMM implementation (delta learning)
+ * Hybrid DFTB+ + TensorFlow QMMM implementation (delta learning)
  */
 
 #include "config.h"
+#include "gromacs/math/vec.h"
 #include "gromacs/utility/smalloc.h"
 
 // When not built in a configuration with QMMM support, much of this
@@ -13,12 +14,13 @@
 #if GMX_QMMM_DFTBPLUS_TENSORFLOW
 
 #include "gromacs/mdlib/qm_dftbplus_tensorflow.h"
+#include <omp.h>
 
 void init_dftbplus_tensorflow(QMMM_QMrec* qm, QMMM_rec* qr, const t_inputrec* ir, const t_commrec* cr)
 {
     // Initialize DFTB+
     init_dftbplus(qm, qr, ir, cr);
-    // Initialize PyTorch
+    // Initialize TensorFlow
     init_tensorflow(qm);
 }
 
@@ -32,22 +34,52 @@ real call_dftbplus_tensorflow(QMMM_rec* qr,
                            gmx_wallcycle_t wcycle)
 {
     int nQMatoms = qm->nrQMatoms_get();
-
-    // Call DFTB+ for base energy/forces
-    real e_dftb = call_dftbplus(qr, cr, qm, mm, f, fshift, nrnb, wcycle);
-    // Store DFTB+ forces for later correction
+    real e_dftb = 0.0;
+    real e_delta = 0.0;
+    
+    // Allocate memory for both force arrays
     rvec* f_dftb = nullptr;
+    rvec* f_delta = nullptr;
     snew(f_dftb, nQMatoms);
+    snew(f_delta, nQMatoms);
+    
+    for (int i=0; i < nQMatoms; i++)
+    {
+        clear_rvec(f_dftb[i]);
+        clear_rvec(f_delta[i]);
+    }
+
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+            // Task for DFTB+ calculation
+            #pragma omp task shared(e_dftb, f_dftb)
+            {
+                // Call DFTB+ for base energy/forces (directly storing in f_dftb)
+                e_dftb = call_dftbplus(qr, cr, qm, mm, f_dftb, fshift, nrnb, wcycle);
+            }
+            
+            // Task for TensorFlow calculation
+            #pragma omp task shared(e_delta, f_delta)
+            {
+                // Call TensorFlow for delta correction (using f_delta)
+                e_delta = call_tensorflow(qr, cr, qm, mm, f_delta, fshift, nrnb, wcycle);
+            }
+            
+            // Wait for both tasks to complete
+            #pragma omp taskwait
+        }
+    }
+    
+    // Combine the forces
     for (int i = 0; i < nQMatoms; ++i)
         for (int d = 0; d < DIM; ++d)
-            f_dftb[i][d] = f[i][d];
-    // Call PyTorch for delta correction
-    real e_delta = call_tensorflow(qr, cr, qm, mm, f, fshift, nrnb, wcycle);
-    // Add delta correction to DFTB+ forces
-    for (int i = 0; i < nQMatoms; ++i)
-        for (int d = 0; d < DIM; ++d)
-            f[i][d] += f_dftb[i][d];
+            f[i][d] = f_dftb[i][d] + f_delta[i][d];
+    
     sfree(f_dftb);
+    sfree(f_delta);
+    
     // Return total energy (DFTB+ + delta)
     return e_dftb + e_delta;
 }
